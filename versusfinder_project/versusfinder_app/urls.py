f092e5da726ab6e3a3fe9931a43d49a402c332e8
quentin.michel <quentin.michel@he-arc.ch>
2019-02-21 17:30:43 +0100

    add some route
    in comment for now

--- versusfinder_project/versusfinder_app/urls.py
+++ versusfinder_project/versusfinder_app/urls.py
@@ -5,4 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('login', views.LoginView.as_view(), name='login'),
+    #path('profile', views.ProfileView.as_view(), name='profile'),
+    #path('dashboard', views.DashboardView.as_view(), name='dashboard'),
+    #path('dashboard/timetable', views.TimetableView.as_view(), name='timetable'),
+    #path('dashboard/timetable/new', views.TimetableView.as_view(), name='timetable-new'),
+    #path('gamepage', views.GamePageView.as_view(), name='gamepage'),
+    #path('searchmatch', views.SearchMatchView.as_view(), name='searchmatch'),
 ]
