d15f4753db8ff0409421ef0b49ef1a5c7e554f91
skogarmadr <luca.srdjenovic@gmail.com>
2019-02-21 16:15:58 +0100

    remove versusfinder_env

--- versusfinder_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\quentin.michel\documents\hearc\annee-3\devweb\versusfinder\versus-finder\versusfinder_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
