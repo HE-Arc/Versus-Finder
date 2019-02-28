e0b234acf03b9ad442560953c678a01111687e0e
Maering <salomon.segan@gmail.com>
2019-02-28 17:41:34 +0100

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,8 +1,13 @@
 @ECHO OFF
 cd versusfinder_project
+python.exe .\manage.py makemigrations versusfinder_app
 python.exe .\manage.py migrate
+python.exe .\manage.py loaddata users.json
 python.exe .\manage.py loaddata game.json
 python.exe .\manage.py loaddata fighters.json
+python.exe .\manage.py loaddata timetable.json
+python.exe .\manage.py loaddata usertimetable.json
+python.exe .\manage.py loaddata match.json
 python.exe .\manage.py runserver
 
 PAUSE
