09c25db775dd7a12f466eec1643cd1ad5eea198f
quentin.michel <quentin.michel@he-arc.ch>
2019-03-07 10:05:38 +0100

    it works TY GOD Steven
    <3

--- versusfinder_project/versusfinder_app/models.py
+++ versusfinder_project/versusfinder_app/models.py
@@ -43,6 +43,9 @@ class UserCharacterBanList(models.Model):
     # created_at = models.DateTimeField(auto_now_add=True, blank=True)
     # updated_at = models.DateTimeField(default=datetime.now, blank=True)
 
+    def __str__(self):
+        return self.character.name
+
 
 class UserGameProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
