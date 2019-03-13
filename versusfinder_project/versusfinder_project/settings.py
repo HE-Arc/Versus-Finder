98c007451783b2aa36a5a2191e14d2b4907ac85d
Maering <salomon.segan@gmail.com>
2019-03-13 13:17:56 +0100

    update checkbox

--- versusfinder_project/versusfinder_project/settings.py
+++ versusfinder_project/versusfinder_project/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+# GODLIKE : https://stackoverflow.com/a/17080877
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
