e0c0d5c40cd035d97a6d0ffd7360522aa181935c
quentin.michel <quentin.michel@he-arc.ch>
2019-03-14 14:55:45 +0100

    change model to be logic and good works on alterbanlist and everywhere

--- versusfinder_project/versusfinder_app/models.py
+++ versusfinder_project/versusfinder_app/models.py
@@ -41,20 +41,20 @@ class Timetable(models.Model):
         return "Start at : "+self.date_begin.strftime("%Y-%m-%d %H:%M:%S")+" | end at : "+self.date_end.strftime("%Y-%m-%d %H:%M:%S")
 
 
-class UserCharacterBanList(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+#class UserCharacterBanList(models.Model):
+    #    character = models.ForeignKey(Character, on_delete=models.CASCADE)
     # created_at = models.DateTimeField(auto_now_add=True, blank=True)
     # updated_at = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return self.character.name
+    #
+    #def __str__(self):
+#   return self.character.name
 
 
 class UserGameProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    mainchar = models.ForeignKey(Character, on_delete=models.CASCADE)
-    banlist = models.ManyToManyField(UserCharacterBanList)
+    mainchar = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='mainchar')
+    banlist = models.ManyToManyField(Character, related_name='banlist')
     username = models.CharField(max_length=200)
     battletag = models.CharField(max_length=200)
     skill_level = models.IntegerField()
