081989d2d0c7532870c8506f646371f0a637e605
skogarmadr <luca.srdjenovic@gmail.com>
2019-04-29 20:53:28 +0200

    Test

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
     }
     stage('QualityTest') { 
             steps {
-        sh './sonar-scanner-3.3.0.1492-linux/bin/sonar-scanner -h'
+        sh './jenkins.sh'
         
 	    }
     }
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,2 @@
-@ECHO OFF
 
-sonar-scanner-3.3.0.1492-linux/bin/sonar-scanner -Dsonar.projectKey=Versusfinder -Dsonar.organization=skogarmadr-github -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io \
--Dsonar.login=c21936d712b162805304fa999c443ef933b4b24
-
-PAUSE
+./sonar-scanner-3.3.0.1492-linux/bin/sonar-scanner -h
