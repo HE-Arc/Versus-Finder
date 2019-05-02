2c6f1799836f853cd054ac9f43c6576c418008d5
skogarmadr <luca.srdjenovic@gmail.com>
2019-05-02 22:23:43 +0200

    New Katalon test

--- runTest.sh
+++ runTest.sh
@@ -2,4 +2,4 @@
 
 Xvfb :99 &
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash -runMode=console -projectPath="$(pwd)/versusfindertest/versusfindertest.prj" -retry=1 -testSuitePath="Test Suites/Halt_Test" -executionProfile="default"  -browserType="Chrome (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash -runMode=console -projectPath="$(pwd)/versusfindertest/versusfindertest.prj" -retry=1 -testSuitePath="Test Suites/Halt_test" -executionProfile="default"  -browserType="Chrome (headless)" -Djava.awt.headless
